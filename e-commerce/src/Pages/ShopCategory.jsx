5116f6027f445f467d2eea9f1548a45e54fda980
Canaga81 <Canagamemmedov@gmail.com>
2023-12-05 21:27:17 +0400

    Dropdown Icon

--- e-commerce/src/Pages/ShopCategory.jsx
+++ e-commerce/src/Pages/ShopCategory.jsx
@@ -1,6 +1,7 @@
 import './CSS/ShopCategory.css'
 import ShopContext from '../Context/ShopContext'
 import { useContext } from 'react'
+import drop_dowwn from '../Components/Assets/dropdown_icon.png'
 
 const ShopCategory = (props) => {
 
@@ -9,6 +10,16 @@ const ShopCategory = (props) => {
   return (
     <div className="shop-category">
       <img src={props.banner} alt="shop_image" />
+
+      <div className="shopcategory-indexSort">
+        <p>
+          <span>Showing 1-12</span> out of 36 Products
+        </p>
+      </div>
+
+      <div className="shopcategory-sort">
+        Sort by <img src={drop_dowwn} alt="dropdown_icon" />
+      </div>
     </div>
   )
 
