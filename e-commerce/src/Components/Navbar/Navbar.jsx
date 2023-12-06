1e06b77fe88c9172b0227ee7cc85b64510761a3f
Canaga81 <Canagamemmedov@gmail.com>
2023-12-06 20:30:06 +0400

    Navbar 2

--- e-commerce/src/Components/Navbar/Navbar.jsx
+++ e-commerce/src/Components/Navbar/Navbar.jsx
@@ -8,31 +8,31 @@ const Navbar = () => {
 
     const [menu, setMenu] = useState('shop')
 
-  return (
+    return (
 
-    <div className='navbar'>
+        <div className='navbar'>
 
-        <div className="nav-logo">
-            <img src={logo} alt="nav-logo" />
-            <p>SHOPPER</p>
+            <div className="nav-logo">
+                <img src={logo} alt="nav-logo" />
+                <p>SHOPPER</p>
+            </div>
+
+            <ul className="nav-menu">
+                <li onClick={() => { setMenu('shop') }}><Link to='/'>Shop</Link> {menu === 'shop' ? <hr /> : <> </>}</li>
+                <li onClick={() => { setMenu('mens') }}><Link to='/mens'>Men</Link> {menu === 'mens' ? <hr /> : <> </>}</li>
+                <li onClick={() => { setMenu('womens') }}><Link to='/womens'>Women</Link> {menu === 'womens' ? <hr /> : <> </>}</li>
+                <li onClick={() => { setMenu('kids') }}><Link to='/kids'>Kids</Link> {menu === 'kids' ? <hr /> : <> </>}</li>
+            </ul>
+
+            <div className="nav-login-cart">
+                <Link to='/login'><button>Login</button></Link>
+                <Link to='/cart'><img src={cart_icon} alt="cart-icon" /></Link>
+                <div className="nav-cart-count">0</div>
+            </div>
+
         </div>
 
-        <ul className="nav-menu">
-            <li onClick={() => {setMenu('shop')}}><Link to='/'>Shop</Link> {menu === 'shop' ? <hr /> : <> </>}</li>
-            <li onClick={() => {setMenu('mens')}}><Link to='/mens'>Men</Link> {menu === 'mens' ? <hr /> : <> </>}</li>
-            <li onClick={() => {setMenu('womens')}}><Link to='/womens'>Women</Link> {menu === 'womens' ? <hr /> : <> </>}</li>
-            <li onClick={() => {setMenu('kids')}}><Link to='/kids'>Kids</Link> {menu === 'kids' ? <hr /> : <> </>}</li>
-        </ul>
-
-        <div className="nav-login-cart">
-            <Link to='/login'><button>Login</button></Link>
-            <Link to='/cart'><img src={cart_icon} alt="cart-icon" /></Link>
-            <div className="nav-cart-count">0</div>
-        </div>
-
-    </div>
-
-  )
+    )
 
 }
 
